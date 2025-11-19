1b109f4990b8124ed33a36c2fd83393a14978022
kazungu-bit <amorganlex468@gmail.com>
2025-11-19 12:06:16 +0300

    add GET and POST endpoints for tasks with in-memory storage

--- app.py
+++
@@ -1,9 +0,0 @@
-
-from flask import Flask
-app =Flask(__name__)
-@app.route('/')
-def hello__world():
- return 'Hello , World! This is my first web server'
-if __name__ == '__main__':
-  app.run(debug=True)
-  
